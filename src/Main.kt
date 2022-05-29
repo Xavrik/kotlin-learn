74635da6670c0edd8bcb23338c902d0c464eea95
Anton <anton.hoahnov@gmail.com>
2022-05-29 15:59:34 +0400

    first commit

---
+++ src/Main.kt
@@ -0,0 +1,83 @@
+var root : String = "Root"
+
+fun main(args: Array<String>){
+
+//    println("Hi Kotlin");
+//    println("Hi guy")
+//
+//    var age: Int = 23
+//    println("Возраст $age" )
+//
+//    age = 32
+//    println(age)
+//
+//    var name : String = "Leo"
+//
+//    var firstChar : Char = 'A'
+//
+//    var isTrue : Boolean = false
+//
+//    var a: Int = -34
+//    var char: Char = 'R'
+//    var fl : Float = 23.093433f
+//    var b : Int = 40000
+//    var bool : Boolean = true
+//    var c : Int? = null
+//    val kons : Int = 45
+//    root = "type"
+
+//    var world: String? = null
+//    var isHasCard = true
+//
+//    world = "Hello"
+
+//    if(world == null){
+//        println("Переменная world равна значению $world")
+//    }else if(world.equals("Hello")){
+//        println("Var $world")
+//    }
+//
+//    else{
+//        println("Переменная world не равна значению $world")
+//    }
+//
+//   val number  = 2
+////
+////    when(number){
+////        null -> print("Переменнтая равна нулю")
+////        10 -> print("Переменнтая равна 10")
+////        5 -> print("Переменнтая равна 5")
+////        else -> print("unfordable")
+////    }
+//
+//var res = if(number > 2) 1 else 0
+//    println(res)
+//
+//    val words = when(number){
+//        null -> "Nothing"
+//        else -> number
+//    }
+//
+//    println("word: " + words)
+//
+////
+//    var a : Int? = 2;
+//    var b : Int? = 3;
+//    val range = 1 .. 100
+//
+//    if (a != null) {
+//        if (a % 2 == 0){
+//            println("четное $a");
+//        } else println("четное $b")
+//    }
+
+    var a : Int = 2
+    when (a){
+        3 -> println("Значения равно 3")
+        7 -> println("Значения равно 7")
+        else  -> println("Другое число")
+    }
+
+
+}
+
